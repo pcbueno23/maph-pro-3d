251ac2a573af40c4f684cc4346894061d1eb081e
pcbueno23 <pcbueno23@gmail.com>
2026-03-29 02:58:20 -0300

    fix(analytics): usar dangerouslySetInnerHTML no script GA4 para evitar SyntaxError

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,12 +35,18 @@ export default function RootLayout({
               src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
               strategy="afterInteractive"
             />
-            <Script id="ga-init" strategy="afterInteractive">{`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_ID}', { page_path: window.location.pathname });
-            `}</Script>
+            <Script
+              id="ga-init"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_ID}');
+                `,
+              }}
+            />
           </>
         ) : null}
         <AuthGuard>
